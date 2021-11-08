5f29338ed2c72cb3e5b17692dcac86b56070d8b7
SebastianKrygier <sebastian.krygier@outlook.com>
2021-11-08 10:53:02 +0100

    test

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent> ();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+               
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+   
+
+
+}
